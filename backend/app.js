43f17d2a532890cda4b70a488fd1cf1356ffb58d
kuchimpos <kuchimpos@g.ucla.edu>
2026-05-26 11:16:53 -0700

    Create Conversation route
    Create conversation route to initiate conversation between two users. Contains only POST route for now, and senderId is sent via body. In the future, this should be sent via authentication middleware (once we have it implemented). Created conversations.js in db folder to hold utility file for storing conversations in Firebase.

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import { router as spotifyRouter } from "./routes/spotify.js";
 import { router as callbackRouter } from "./routes/callback.js";
+import { router as conversationsRouter } from "./routes/conversations.js";
 
 dotenv.config();
 
@@ -13,6 +14,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/spotify", spotifyRouter);
 app.use("/callback", callbackRouter);
+app.use("/conversations", conversationsRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
