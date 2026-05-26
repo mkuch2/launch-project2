e2c3e20df5f1ab1439bda0feb94c4aeaddf06d00
kuchimpos <kuchimpos@g.ucla.edu>
2026-05-26 12:04:55 -0700

    Update Conversations route to fit new data schema
    Added last_message object to conversatinos to keep track of message ordering via sent_at timestamp. Updated createConversation helper utility to take in last_message object and store in database. Also added participant id checking to prevent duplicate conversations between the same users.

--- backend/routes/conversations.js
+++ backend/routes/conversations.js
@@ -5,12 +5,18 @@ const router = express.Router();
 
 // Conversations have the form:
 // { id: string,
-//   participants: [string, string]
+//   participants: [string, string],
+//   last_message: {
+//                   content: string,
+//                   read: bool,
+//                   sender_id: string,
+//                   sent_at: timestamp
+//                 }
 //  }
 
 router.post("/", async (req, res) => {
   // TODO: get sender with auth middleware rather than body const senderId = req.user.id;
-  const { senderId, recipientId } = req.body;
+  const { senderId, recipientId, initialMessage } = req.body;
 
   if (!senderId) {
     return res.status(400).json({
@@ -18,16 +24,27 @@ router.post("/", async (req, res) => {
     });
   }
 
-  if (!recipientId) {
+  if (!recipientId || !initialMessage) {
     return res.status(400).json({
-      message: "recipientId is required to create a new conversation",
+      message:
+        "recipientId and initialMessage are required to create a new conversation",
     });
   }
 
   try {
-    const newConversation = await createConversation(senderId, recipientId);
+    const newConversation = await createConversation(
+      senderId,
+      recipientId,
+      initialMessage,
+    );
     return res.status(201).json(newConversation);
   } catch (err) {
+    if (err.code === "CONVERSATION_ALREADY_EXISTS") {
+      return res.status(409).json({
+        message: "Conversation already exists between these users",
+      });
+    }
+
     console.error("Error when attempting to create new conversation: ", err);
     return res.status(500).json({
       message: "Error occurred when creating conversation. Please try again.",
