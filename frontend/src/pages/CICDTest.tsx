fda12835c2fade670796c36136480f2685fce33c
VincentChan22 <VincentHChan22@gmail.com>
2026-05-29 12:33:16 -0400

    created a test file for CICD

---
+++ frontend/src/pages/CICDTest.tsx
@@ -0,0 +1,15 @@
+import { useState, useEffect, useContext } from "react"; // unused imports
+
+export default async function CICDTest() {
+    try {
+        const response = await fetch("http://localhost:5005/api/users", {
+          credentials: "include",
+        });
+        const data : any = await response.json(); // unused data and any type
+    }
+    catch (err) {
+        console.error("Error in CICDTest:"); // no cause set for error
+    }
+    return <>
+    </>
+}
