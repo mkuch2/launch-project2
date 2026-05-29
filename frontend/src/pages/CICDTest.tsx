bdc3aedbe12ea9c07a6a440ab7e89eaa6ee90d7d
VincentChan22 <VincentHChan22@gmail.com>
2026-05-29 12:35:33 -0400

    changed lint error of error handeling

--- frontend/src/pages/CICDTest.tsx
+++ frontend/src/pages/CICDTest.tsx
@@ -8,7 +8,7 @@ export default async function CICDTest() {
         const data : any = await response.json(); // unused data and any type
     }
     catch (err) {
-        console.error("Error in CICDTest:"); // no cause set for error
+        console.error("Error in CICDTest: "+err); // no cause set for error
     }
     return <>
     </>
